ae8cbbe16ba13b387212ab7c96eb0041a2e19814
izarubin <izarubin@neoflex.ru>
2020-10-18 15:19:59 +0300

    print init Commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,7 @@
 public class MainClass {
 
     public static void main(String[] args) {
-
+        PrinterCommits commitInit = new PrinterCommits("init commit");
+        commitInit.printCommit();
     }
 }
