970c08f19823bd18bf3a1293b26f639efba7de70
izarubin <izarubin@neoflex.ru>
2020-10-18 16:20:40 +0300

    realized Random

---
+++ src/randon/GenerateRandon.java
@@ -0,0 +1,11 @@
+package randon;
+
+import java.util.Random;
+
+public class GenerateRandon {
+    private static Random random = new Random();
+
+    public static int generate(){
+        return random.nextInt(30);
+    }
+}
