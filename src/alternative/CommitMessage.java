fcdac60720960c4136c7a65eade1867468fc03b4
izarubin <izarubin@neoflex.ru>
2020-10-18 15:23:51 +0300

    create commitMessage class

---
+++ src/alternative/CommitMessage.java
@@ -0,0 +1,13 @@
+package alternative;
+
+public class CommitMessage {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
