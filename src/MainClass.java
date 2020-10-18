2ccf3d90ca428535e7d55b92b6a15d0f05c0a073
izarubin <izarubin@neoflex.ru>
2020-10-18 16:35:28 +0300

    realize expluatate

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,16 @@
+import alternative.AlternativePrinterCommits;
+import alternative.CommitMessage;
+import calculate.CalculaterImpl;
+import randon.GenerateRandon;
+
 public class MainClass {
 
     public static void main(String[] args) {
-        PrinterCommits commitInit = new PrinterCommits("init commit");
-        commitInit.printCommit();
+        CommitMessage commitMessage = new CommitMessage();
+        commitMessage.setMessage("Commit message");
+        AlternativePrinterCommits alternativePrinterCommits = new AlternativePrinterCommits(commitMessage);
+        CalculaterImpl calculater = new CalculaterImpl();
+        alternativePrinterCommits.printCommit();
+        System.out.println(calculater.sumAndSub(GenerateRandon.generate(), GenerateRandon.generate()));
     }
 }
